c9ac2d3e56c4721a21320f9127884dcc3c4d1678
Marcos <marcoslvazq@gmail.com>
2023-01-27 20:09:47 -0300

    Locura

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -140,7 +140,7 @@ function buscoInterseccion(array1, array2) {
   var def = [];
   for (let i = 0; i < array1.length; i++) {
     for (let j = 0; j < array2.length; j++) {
-      if ((array1[i] = array2[j] && !newArr.includes(array1[i]))) {
+      if ((array1[i] === array2[j] && !newArr.includes(array1[i]))) {
         newArr.push(array1[i]);
       }
     }
