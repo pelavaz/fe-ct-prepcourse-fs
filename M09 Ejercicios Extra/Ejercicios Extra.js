377c20f43e124d8f49e74d70578074bd0cf6b28d
Marcos Vazquez <111909382+pelavaz@users.noreply.github.com>
2023-01-30 12:21:15 -0300

    Update Ejercicios Extra.js

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -163,3 +163,48 @@ module.exports = {
   sortArray,
   buscoInterseccion,
 };
+* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function soloNumeros(array) {
+  // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
+  // Debe devolver un arreglo con solo los enteros.
+  // Ej: 
+  // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
+
+  // Tu código aca:
+  var arrNuevo = []
+  for(let i=0; i<array.length;i++){
+    if(typeof(array[i])==='number'){
+      arrNuevo.push(array[i])
+    }
+  }
+  return arrNuevo
+
+}
+
+function stringMasLarga(strings) {
+  // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
+  // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
+  // Ej:
+  // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
+  // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
+
+  // Tu código aca
+  var arr = [];
+  for (let m = 0; m < strings.length; m++) {
+    arr.push({ name: strings[m], value: strings[m].length });
+  }
+
+  arr.sort((a, b) => a.value - b.value);
+
+
+  return arr[arr.length -1].name
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = stringMasLarga
