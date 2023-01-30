024e8fd1e8959525c958d517f21deea28cf2ae47
Marcos Vazquez <111909382+pelavaz@users.noreply.github.com>
2023-01-30 14:25:09 -0300

    Update Ejercicios Extra.js

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -369,3 +369,50 @@ return suma/this.amigos.length
 // No modifiques nada debajo de esta linea //
 
 module.exports = crearClasePersona
+
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function filtrar(funcion) {
+  // Escribi una función filtrar en el prototipo de Arrays,
+  // que recibe una funcion (callback) que devuelve true o false.
+  // filtrar los elementos de ese arreglo en base al resultado de esa funcion
+  // comparadora, devolver un nuevo arreglo con los elementos filtrados.
+  // NO USAR LA FUNCION FILTER DE LOS ARREGLOS.
+  // ej:
+  // var productos = [{
+  //   price: 100,
+  //   name: 'tv'
+  // }, {
+  //   price: 50,
+  //   name: 'phone'
+  // }, {
+  //   price: 30,
+  //   name: 'lamp'
+  // }]
+  // productos.filtrar(function(p) {
+  //   return p.price >= 50;
+  // }) => [{price: 100, name:'tv'}]
+
+  Array.prototype.filtrar = function(cb){
+
+  
+    var arregloModificado = [];
+    for (var i = 0; i < this.length; i++) {
+         if (cb(this[i])) {
+              arregloModificado.push(this[i]);
+         } 
+    }
+    return arregloModificado;
+ };
+  
+
+
+};
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = filtrar
