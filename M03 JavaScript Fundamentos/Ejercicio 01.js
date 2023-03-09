9be486638722dad20e3c68c3b006ef9d7f259383
Marcos <marcoslvazq@gmail.com>
2023-03-09 13:19:33 -0300

    Ultimo

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -8,7 +8,7 @@ const nuevoString = "Pepito";
 const nuevoNumero = 9;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = true; // jdjskadjkdjaks
+const nuevoBoolean = true; // jdjskadjkdjaksfsdgfsdgsg
 
 // Resuelve el siguiente problema matemático.
 const nuevaResta = 10 - 7 === 3;
